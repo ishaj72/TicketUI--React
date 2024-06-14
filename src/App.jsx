abd650a2dd611e26e0a496ac7628f6233e9990ec
ishaj72 <isha30435@gmail.com>
2024-06-14 22:11:04 +0530

    adding registeration page jsx and css files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './Registeration.css'
+import Registration from './Registration'
+
+function App() {
+  return (
+    <div className='app'>
+      <header className='app-header'>
+        <h1>Registeration Form</h1>
+        <Registration/>
+      </header>
+    </div>
+  )
+}
+
+export default App
