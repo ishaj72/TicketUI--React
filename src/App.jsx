89b77147babc5842b03f2dcf918e6c9c8c399d31
ishaj72 <isha30435@gmail.com>
2024-06-15 13:57:45 +0530

    commit registration page

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,18 @@ function App() {
   return (
     <div className='app'>
       <header className='app-header'>
-        <h1>Hello!! Registeration Yourself and be our new member</h1>
-        <Registration/>
+        <div className='left'>
+          <h1>Ticketify</h1>
+          <Registration />
+        </div>
+        <div className='right'>
+          <div className='overlay'>
+            <p className='overlay-text'>Welcome<br />Get yourself registered with us</p>
+          </div>
+        </div>
       </header>
     </div>
-  )
+  );
 }
 
 export default App
